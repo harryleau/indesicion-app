05ea44800528a3366d660f62c75b33b7ee47c781
Harry <harryle.au@gmail.com>
2018-06-08 20:13:32 +1000

    convert some components to stateless, handle input validation for addOption, add state for components

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,26 +8,193 @@ function _possibleConstructorReturn(self, call) { if (!self) { throw new Referen
 
 function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
 
-var Toggle = function (_React$Component) {
-  _inherits(Toggle, _React$Component);
+var IndecisionApp = function (_React$Component) {
+  _inherits(IndecisionApp, _React$Component);
 
-  function Toggle(props) {
-    _classCallCheck(this, Toggle);
+  function IndecisionApp(props) {
+    _classCallCheck(this, IndecisionApp);
 
-    var _this = _possibleConstructorReturn(this, (Toggle.__proto__ || Object.getPrototypeOf(Toggle)).call(this, props));
+    var _this = _possibleConstructorReturn(this, (IndecisionApp.__proto__ || Object.getPrototypeOf(IndecisionApp)).call(this, props));
 
-    _this.toggle = _this.toggle.bind(_this);
+    _this.handleDeleteOptions = _this.handleDeleteOptions.bind(_this);
+    _this.handlePick = _this.handlePick.bind(_this);
+    _this.handleAddOption = _this.handleAddOption.bind(_this);
     _this.state = {
-      visibility: false
+      options: []
     };
     return _this;
   }
 
-  _createClass(Toggle, [{
-    key: 'toggle',
-    value: function toggle() {
+  _createClass(IndecisionApp, [{
+    key: 'handleDeleteOptions',
+    value: function handleDeleteOptions() {
+      this.setState(function () {
+        return { options: [] };
+      });
+    }
+  }, {
+    key: 'handlePick',
+    value: function handlePick() {
+      var optionIndex = Math.floor(Math.random() * this.state.options.length);
+      var option = this.state.options[optionIndex];
+      alert(option);
+    }
+
+    // we can use push as prevState.options.push(option). But we should not manipulate prevState => use concat.
+
+  }, {
+    key: 'handleAddOption',
+    value: function handleAddOption(option) {
+      if (!option) {
+        return 'Enter valid value to add item';
+      } else if (this.state.options.indexOf(option) > -1) {
+        return 'This option already exists';
+      }
+
       this.setState(function (prevState) {
-        return { visibility: !prevState.visibility };
+        return {
+          options: prevState.options.concat([option])
+        };
+      });
+    }
+
+    // props passed down to child components are read-only => cannot change values of options props in Options component => pass another props handleDeleteOptions down => because when a props is trigger and change states values, it will make re-render the whole component. 
+    // in this case, handleDeleteOptions change the options state value => re-render Options => empty the options array. 
+
+  }, {
+    key: 'render',
+    value: function render() {
+      var title = 'Indecision';
+      var sub = 'Put your life in the hands of a computer';
+      return React.createElement(
+        'div',
+        null,
+        React.createElement(Header, { title: title, sub: sub }),
+        React.createElement(Action, {
+          hasOptions: this.state.options.length > 0,
+          handlePick: this.handlePick
+        }),
+        React.createElement(Options, {
+          options: this.state.options,
+          handleDeleteOptions: this.handleDeleteOptions
+        }),
+        React.createElement(AddOption, { handleAddOption: this.handleAddOption })
+      );
+    }
+  }]);
+
+  return IndecisionApp;
+}(React.Component);
+
+// stateless component
+
+
+var Header = function Header(props) {
+  return React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'h1',
+      null,
+      props.title
+    ),
+    React.createElement(
+      'h4',
+      null,
+      props.sub
+    )
+  );
+};
+
+var Action = function Action(props) {
+  return React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'button',
+      {
+        onClick: props.handlePick,
+        disabled: !props.hasOptions
+      },
+      'What should I do?'
+    )
+  );
+};
+
+// old syntax, since we dont need any state, we can use stateless components
+/*
+class Action extends React.Component {
+  render() {
+    return (
+      <div>
+        <button 
+          onClick={this.props.handlePick}
+          disabled={!this.props.hasOptions}
+        >
+          What should I do?
+        </button>
+      </div>
+    );
+  }
+}
+*/
+
+// we can't use a method of class in jxs render because it already lost the context of 'this' => use bind() to add the context of 'this' to methods.
+// We can bind method right in jsx but that's not efficient as we have to do it multiple times => use constructor
+// props in constructor is this.props that we use in jsx 
+
+var Options = function Options(props) {
+  return React.createElement(
+    'div',
+    null,
+    React.createElement(
+      'button',
+      { onClick: props.handleDeleteOptions },
+      'Remove All'
+    ),
+    props.options.map(function (option) {
+      return React.createElement(Option, { key: option, optionText: option });
+    })
+  );
+};
+
+var Option = function Option(props) {
+  return React.createElement(
+    'div',
+    null,
+    props.optionText
+  );
+};
+
+// we have to keep the handleAddOption down here because we want to do something with it (get the value) => we have to bind 2 times to keep the context of 'this'.
+// we have to add option to options array above because props are read-only. 
+
+var AddOption = function (_React$Component2) {
+  _inherits(AddOption, _React$Component2);
+
+  function AddOption(props) {
+    _classCallCheck(this, AddOption);
+
+    var _this2 = _possibleConstructorReturn(this, (AddOption.__proto__ || Object.getPrototypeOf(AddOption)).call(this, props));
+
+    _this2.handleAddOption = _this2.handleAddOption.bind(_this2);
+    _this2.state = {
+      error: undefined
+    };
+    return _this2;
+  }
+
+  _createClass(AddOption, [{
+    key: 'handleAddOption',
+    value: function handleAddOption(e) {
+      e.preventDefault();
+
+      var option = e.target.elements.option.value.trim();
+      e.target.elements.option.value = '';
+      var error = this.props.handleAddOption(option);
+      // if option is successfully passed, error will be undefined because in that case, options array has been changed and it will re-render => error will not be passed.
+      this.setState(function () {
+        return { error: error };
       });
     }
   }, {
@@ -36,57 +203,40 @@ var Toggle = function (_React$Component) {
       return React.createElement(
         'div',
         null,
-        React.createElement(
-          'h1',
-          null,
-          'Visibility Toggle'
+        this.state.error && React.createElement(
+          'p',
+          { className: 'text-danger' },
+          this.state.error
         ),
         React.createElement(
-          'button',
-          { onClick: this.toggle },
-          this.state.visibility ? 'Hide details' : 'Show details'
-        ),
-        this.state.visibility && React.createElement(
-          'div',
-          null,
+          'form',
+          { onSubmit: this.handleAddOption },
+          React.createElement('input', { type: 'text', name: 'option' }),
           React.createElement(
-            'p',
+            'button',
             null,
-            'These are some details.'
+            'Add Option'
           )
         )
       );
     }
   }]);
 
-  return Toggle;
+  return AddOption;
 }(React.Component);
 
-ReactDOM.render(React.createElement(Toggle, null), document.getElementById('app'));
+ReactDOM.render(React.createElement(IndecisionApp, null), document.getElementById('app'));
 
-// let visibility = false;
+// stateless component example
+/*
+const User = (props) => {
+  return (
+    <div>
+      <p>Name: {props.name}</p>
+      <p>Age: {props.age}</p>
+    </div>
+  );
+};
 
-// const toggle = () => {
-//   visibility = !visibility;
-//   render();
-// };
-
-// const render = () => {
-//   const jsx = (
-//     <div>
-//       <h1>Visibility Toggle</h1>
-//       <button onClick={toggle}>
-//         {visibility ? 'Hide details' : 'Show details'}
-//       </button>
-//       {visibility && (
-//         <div>
-//           <p>These are some details.</p>
-//         </div>
-//       )}
-//     </div>
-//   );
-
-//   ReactDOM.render(jsx, document.getElementById('app'));
-// };
-
-// render();
+ReactDOM.render(<User name="Harry" age={24} />, document.getElementById('app'));
+*/
--- src/app.js
+++ src/app.js
@@ -1,92 +1,155 @@
 class IndecisionApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleDeleteOptions = this.handleDeleteOptions.bind(this);
+    this.handlePick = this.handlePick.bind(this);
+    this.handleAddOption = this.handleAddOption.bind(this);
+    this.state = {
+      options: []
+    }
+  }
+
+  handleDeleteOptions() {
+    this.setState(() => {
+      return { options: [] };
+    });
+  }
+
+  handlePick() {
+    const optionIndex = Math.floor(Math.random() * this.state.options.length);
+    const option = this.state.options[optionIndex]; 
+    alert(option);
+  }
+
+  // we can use push as prevState.options.push(option). But we should not manipulate prevState => use concat.
+  handleAddOption(option) {
+    if(!option) {
+      return 'Enter valid value to add item';
+    } else if (this.state.options.indexOf(option) > -1) {
+      return 'This option already exists';
+    } 
+
+    this.setState((prevState) => {
+      return {
+        options: prevState.options.concat([option])
+      };
+    });
+  }
+
+  // props passed down to child components are read-only => cannot change values of options props in Options component => pass another props handleDeleteOptions down => because when a props is trigger and change states values, it will make re-render the whole component. 
+  // in this case, handleDeleteOptions change the options state value => re-render Options => empty the options array. 
   render() {
     const title = 'Indecision';
     const sub = 'Put your life in the hands of a computer';
-    const options = ['Item 1', 'Item 2', 'Item 4'];
     return (
       <div>
         <Header title={title} sub={sub} />
-        <Action  />
-        <Options options={options} />
-        <AddOption />
+        <Action 
+          hasOptions={this.state.options.length > 0} 
+          handlePick={this.handlePick}
+        />
+        <Options 
+          options={this.state.options} 
+          handleDeleteOptions={this.handleDeleteOptions}
+        />
+        <AddOption handleAddOption={this.handleAddOption} />
       </div>
     );
   }
 }
 
-// Header component in the parent class is just an instance of class Header => 'this' can refer to that instance => this.props can give access to all props we pass to in the parent class.
-class Header extends React.Component {
-  render() {
-    return (
-      <div>
-        <h1>{this.props.title}</h1>
-        <h4>{this.props.sub}</h4>
-      </div>
-    );
-  }
+// stateless component
+const Header = (props) => {
+  return (
+    <div>
+      <h1>{props.title}</h1>
+      <h4>{props.sub}</h4>
+    </div>
+  );
 }
 
+
+const Action = (props) => {
+  return (
+    <div>
+      <button 
+        onClick={props.handlePick}
+        disabled={!props.hasOptions}
+      >
+        What should I do?
+      </button>
+    </div>
+  );
+};
+
+// old syntax, since we dont need any state, we can use stateless components
+/*
 class Action extends React.Component {
-  handlePick() {
-    
-  }
-
   render() {
     return (
       <div>
-        <button onClick={this.handlePick}>What should I do?</button>
+        <button 
+          onClick={this.props.handlePick}
+          disabled={!this.props.hasOptions}
+        >
+          What should I do?
+        </button>
       </div>
     );
   }
 }
+*/
 
 // we can't use a method of class in jxs render because it already lost the context of 'this' => use bind() to add the context of 'this' to methods.
 // We can bind method right in jsx but that's not efficient as we have to do it multiple times => use constructor
 // props in constructor is this.props that we use in jsx 
-class Options extends React.Component {
+
+const Options = (props) => {
+  return (
+    <div>
+      <button onClick={props.handleDeleteOptions}>Remove All</button>
+      {
+        props.options.map(option => <Option key={option} optionText={option} />)
+      }   
+    </div>
+  );
+}
+
+const Option = (props) => {
+  return (
+    <div>
+      {props.optionText}
+    </div>
+  );
+}
+
+// we have to keep the handleAddOption down here because we want to do something with it (get the value) => we have to bind 2 times to keep the context of 'this'.
+// we have to add option to options array above because props are read-only. 
+class AddOption extends React.Component {
   constructor(props) {
     super(props);
-    this.handleRemoveAll = this.handleRemoveAll.bind(this);
-  }
-  handleRemoveAll() {
-    console.log(this.props.options);
+    this.handleAddOption = this.handleAddOption.bind(this);
+    this.state = {
+      error: undefined
+    }
   }
 
-  render() {
-    return (
-      <div>
-        <button onClick={this.handleRemoveAll}>Remove All</button>
-        {
-          this.props.options.map(option => <Option key={option} optionText={option} />)
-        }   
-      </div>
-    );
-  }
-}
-
-class Option extends React.Component {
-  render() {
-    return (
-      <div>
-        {this.props.optionText}
-      </div>
-    );
-  }
-}
-
-class AddOption extends React.Component {
   handleAddOption(e) {
     e.preventDefault();
 
     const option = e.target.elements.option.value.trim();
-    if(option) {
-      alert(option);
-    }
+    e.target.elements.option.value = '';
+    const error = this.props.handleAddOption(option);
+    // if option is successfully passed, error will be undefined because in that case, options array has been changed and it will re-render => error will not be passed.
+    this.setState(() => {
+      return { error };
+    });
   }
 
   render() {
     return (
       <div>
+        {this.state.error && <p className="text-danger">{this.state.error}</p>}
         <form onSubmit={this.handleAddOption}>
           <input type="text" name="option"/>
           <button>Add Option</button>
@@ -97,3 +160,19 @@ class AddOption extends React.Component {
 }
 
 ReactDOM.render(<IndecisionApp />, document.getElementById('app'));
+
+// stateless component example
+/*
+const User = (props) => {
+  return (
+    <div>
+      <p>Name: {props.name}</p>
+      <p>Age: {props.age}</p>
+    </div>
+  );
+};
+
+ReactDOM.render(<User name="Harry" age={24} />, document.getElementById('app'));
+*/
+
+
