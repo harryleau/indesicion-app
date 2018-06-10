3bb955de65448abad22212f5e578417dee50da49
Harry <harryle.au@gmail.com>
2018-06-11 00:44:25 +1000

    add normalize.css for cross-browser issues

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import IndecisionApp from './components/IndecisionApp';
+import 'normalize.css/normalize.css';
+// install and use normalize.css to avoid cross-browser issues, modify webpack config to use both css and scss file
 import './styles/styles.scss';
 // to use css, we have to install css-loader and style-loader, then add them to webpack config file.
 // to use scss, install node-sass, sass-loader, add sass-loader to webpack (sass-loader will run node-sass automatically), and change the file type to .scss.
